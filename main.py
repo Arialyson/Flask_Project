44b21e2a6ecb24c9ec69e71264c556fe5126ff43
Arialyson Lima <arialyson@gmail.com>
2026-01-19 22:44:23 -0300

    Estruturando projeto e renderizando HTML

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    return "Minha página Flask."
 
 if __name__ == '__main__':
     app.run(debug=True)
