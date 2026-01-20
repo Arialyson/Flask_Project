d4fab5c2c71428d3075cad6d254cc5fe118cc08f
Arialyson Lima <arialyson@gmail.com>
2026-01-19 23:20:51 -0300

    passando dados do Servidor

--- app/views.py
+++ app/views.py
@@ -3,7 +3,13 @@ from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = 'Arialyson'
+    cidade = 'Natal-RN'
+    context = {
+        'usuario': usuario,
+        'cidade': cidade
+    }
+    return render_template('index.html', context=context)
 
 @app.route('/nova/')
 def novapag():
